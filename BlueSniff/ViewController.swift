a8c5a2a69c9ab5288a0f2a7221e8f3515515dcd1
Nick Walter <madeupbypeople@gmail.com>
2015-08-06 15:41:21 -0600

    Initial Commit

---
+++ BlueSniff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueSniff
+//
+//  Created by Nick Walter on 8/6/15.
+//  Copyright © 2015 Nick Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
